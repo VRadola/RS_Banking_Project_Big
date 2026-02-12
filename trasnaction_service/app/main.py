2e8106f159d2ca1131d1974664980160fc130641
VRadola <152493489+VRadola@users.noreply.github.com>
2026-02-12 23:29:09 +0100

    Delete trasnaction_service directory

--- trasnaction_service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers.transactions import router as transaction_router
-
-app = FastAPI(title="Transaction Service")
-app.include_router(transaction_router)
-
-app.get("/health")
-def health():
-    return {"status": "ok"}
