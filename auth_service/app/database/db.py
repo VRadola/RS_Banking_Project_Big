ad777d648cc760c0b3d153515b8009865fd0eb13
VRadola <152493489+VRadola@users.noreply.github.com>
2026-02-14 05:31:31 +0100

    Delete auth_service directory

--- auth_service/app/database/db.py
+++
@@ -1,5 +0,0 @@
-import boto3
-from ..config import DDB_USERS_TABLE, AWS_REGION
-
-dynamodb = boto3.resource("dynamodb", region_name = AWS_REGION)
-users_table = dynamodb.Table(DDB_USERS_TABLE)
--- auth_service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .routers.auth import router as auth_router
-
-app = FastAPI(title="Auth_Service")
-app.include_router(auth_router)
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
